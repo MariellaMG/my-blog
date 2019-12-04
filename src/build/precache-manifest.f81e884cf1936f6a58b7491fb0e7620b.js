6a370b90ff5712765c9f43a4b36bbeabcf9b6761
mariella <magallon.mariela5@gmail.com>
2019-12-04 00:59:31 -0800

    First commit

---
+++ src/build/precache-manifest.f81e884cf1936f6a58b7491fb0e7620b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e22f47bad619911e55c1b8aabe3403",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36d3e58745983ddad325",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "055698a16b00539b91ef",
+    "url": "/static/js/2.98b44fe8.chunk.js"
+  },
+  {
+    "revision": "36d3e58745983ddad325",
+    "url": "/static/js/main.177024c0.chunk.js"
+  },
+  {
+    "revision": "35a7acf4fb928453b799",
+    "url": "/static/js/runtime-main.8727d782.js"
+  }
+]);
